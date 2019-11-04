545b2c3c814bb367cd334c17681c02ef9a311105
Esteban Arce <estebana@exo.com.ar>
2019-11-04 11:38:32 -0300

    Final touch

--- drumkit/src/scripts.js
+++ drumkit/src/scripts.js
@@ -1,6 +1,25 @@
 
 
-window.addEventListener('keydown', function(e) {
-    const audio = document.querySelector('audio[data-key="${e.keyCode}"]');
-    console.log(audio);
-});
+    
+
+    function playSound(e) {
+        var audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+        var key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+        if(!audio) return;
+        audio.currentTime = 0;
+        audio.play();
+        key.classList.add('playing');
+}
+
+    function removeTransition(e) {
+        if(e.propertyName !== 'transform') return;
+        this.classList.remove('playing');
+    }
+
+
+    var keys = document.querySelectorAll('.key');
+
+    keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+
+    window.addEventListener('keydown', playSound);
